03af91d68e37b533e21e261da232e35ffd0316da
Muhammad Ahmad <mahmadmalik0301@gmail.com>
2025-09-02 17:59:44 +0500

    intial

--- src/email/email.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { EmailService } from './email.service';
-import { EmailWorker } from './email.worker';
-
-@Module({
-  providers: [EmailService, EmailWorker],
-})
-export class EmailModule {}
